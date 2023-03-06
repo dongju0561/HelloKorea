933104f682848e3a324431b9556b95c34728b399
DongjuPark <pdj95044@gmail.com>
2023-03-06 22:01:17 +0900

    adjust distance of map screen

--- practice_pinOnMap/MapViewController.swift
+++ practice_pinOnMap/MapViewController.swift
@@ -41,12 +41,12 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         view.addSubview(pickerView)
         view.addSubview(mapView)
         
         pickerView.delegate = self
         pickerView.dataSource = self
+        mapView.delegate = self
         
         NSLayoutConstraint.activate([
             
@@ -60,14 +60,9 @@ class MapViewController: UIViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
         
-        //pin을 찍기 위한 코드
-        mapView.delegate = self
-        
         let location = contentsData?.locations
-        let initialLocation = CLLocation(latitude: (location?[0].latitude)!, longitude: (location?[0].longitude)!)
-        //초기 지도 위치
-        mapView.centerToLocation(initialLocation)
         
+        //
         for index in 0..<(contentsData?.locations.count)!{
             let artwork = Artwork(
                 title: location?[index].locationName,
@@ -77,14 +72,13 @@ class MapViewController: UIViewController {
             
             mapView.addAnnotation(artwork)
         }
+        
+        //초기 지도 위치
+        // Define the initial location
+        let initialLocation = CLLocation(latitude: (location?[0].latitude)!, longitude: (location?[0].longitude)!)
 
-//                let leftMargin:CGFloat = 10
-//                let topMargin:CGFloat = 60
-//                let mapWidth:CGFloat = view.frame.size.width-20
-//                let mapHeight:CGFloat = 300
-//
-//                mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
-                
+        // Set the map view to display the region
+        mapView.setLocation(initialLocation)
     }
 }
 
@@ -145,23 +139,17 @@ extension MapViewController: UIPickerViewDelegate, UIPickerViewDataSource{
         let longitude = contentsData?.locations[row].longitude ?? 0
         
         // Create a new coordinate for the center of the map
-        let newCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-
-        // Create a new span that defines how zoomed in the map should be
-        let newSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-
-        // Create a new region that combines the coordinate and the span
-        let newRegion = MKCoordinateRegion(center: newCoordinate, span: newSpan)
+        let newRegion = CLLocation(latitude: latitude, longitude: longitude)
 
         // Move the map to the new region, with animation
-        mapView.setRegion(newRegion, animated: true)
+        mapView.setLocation(newRegion)
     }
 }
 
 private extension MKMapView {
-  func centerToLocation(
+  func setLocation(
     //기존에 있는에 메소드의 argument를 extend operator를 사용하여 상수를 입력해줄 수 있다.
-    _ location: CLLocation, regionRadius: CLLocationDistance = 3000) {
+    _ location: CLLocation, regionRadius: CLLocationDistance = 1000) {
     let coordinateRegion = MKCoordinateRegion( center: location.coordinate,
                                                latitudinalMeters: regionRadius,
                                                longitudinalMeters: regionRadius)
