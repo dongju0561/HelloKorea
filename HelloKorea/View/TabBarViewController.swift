945c3eefcf5c892de90b657efcfb9c56da80decb
DongjuPark <pdj95044@gmail.com>
2023-11-07 21:05:53 +0900

    add tabBar

--- HelloKorea/View/TabBarViewController.swift
+++ HelloKorea/View/TabBarViewController.swift
@@ -1,8 +1,29 @@
 //
 //  TabBarViewController.swift
-//  HelloKorea
+//  practice_pinOnMap
 //
-//  Created by Dongju Park on 11/7/23.
+//  Created by Dongju Park on 2023/03/09.
 //
+import UIKit
 
-import Foundation
+class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let appearance = configureAppearance()
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+
+        tabBar.tintColor = UIColor(named: Color.TabBarTintColor)!
+        tabBar.barTintColor = UIColor(named: Color.TabBarTintColor)!
+
+        delegate = self
+    }
+    func configureAppearance() -> UITabBarAppearance {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: Color.TabBarBackgroundColor)!
+        return appearance
+    }
+}
