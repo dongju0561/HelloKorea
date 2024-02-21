89ba00b3404707362ad2d81bf56b8db869c55a4c
DongjuPark <pdj95044@gmail.com>
2024-02-21 17:31:54 +0900

    firebase Storage에서 이미지 업로드 테스트

--- HelloKorea/View/LoginViewController.swift
+++ HelloKorea/View/LoginViewController.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import FirebaseStorage
 import Firebase
 
-
+//images/swiftui.png
 
 class LoginViewController: UIViewController {
     let storage = Storage.storage()
@@ -21,6 +21,7 @@ class LoginViewController: UIViewController {
     let viewModel = LoginViewModel()
     let disposeBag = DisposeBag()
     
+    
     private let titleLabel: UILabel = {
         var lbl = UILabel()
         lbl.text = "HelloKorea"
@@ -29,7 +30,12 @@ class LoginViewController: UIViewController {
         lbl.font = .systemFont(ofSize: 24)
         return lbl
     }()
-    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false // Auto Layout 사용을 위해 false로 설정합니다.
+        imageView.contentMode = .scaleAspectFit // 이미지의 비율을 유지하면서 이미지뷰에 맞게 조정합니다.
+        return imageView
+    }()
     private let usernameTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Username"
@@ -38,7 +44,6 @@ class LoginViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
-    
     private let passwordTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Password"
@@ -48,7 +53,6 @@ class LoginViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
-    
     private let loginButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Login", for: .normal)
@@ -61,12 +65,50 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupActions()
+        downLoadImage(imagePath: "images/swiftui.png")
+    }
+    private func downLoadImage(imagePath: String){
+        
+        let storageRef = storage.reference(withPath: imagePath)
+        storageRef.downloadURL { [self] (url, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else {
+                // 다운로드 URL이 성공적으로 가져와졌을 때
+                if let imageUrl = url {
+                    // URL을 통해 이미지를 다운로드하거나 사용할 수 있습니다.
+                    // 예를 들어, Alamofire 등의 라이브러리를 사용하여 이미지를 가져올 수 있습니다.
+                    // 이 예시에서는 단순히 URL을 출력하는 것으로 대체합니다.
+                    guard let safeURL = url else {
+                        return
+                    }
+                    let session = URLSession.shared
+                    let request = URLRequest(url: safeURL)
+                    print("print this: \(safeURL)")
+                    let dataObservable = session.rx.data(request: request)
+                    
+                    dataObservable.subscribe (onNext: { data in
+                        
+                        DispatchQueue.main.async {
+                            let image = UIImage(data: data)
+                            self.imageView.image = image
+                        }
+                        
+                    }, onError: { error in
+                        // 에러 처리
+                        print("Error: \(error.localizedDescription)")
+                    }).disposed(by: disposeBag)
+                    print("Download URL: \(imageUrl.absoluteString)")
+                }
+            }
+        }
     }
     
     private func setupUI() {
         view.layer.insertSublayer(setGradient(), at: 0)
         
         view.addSubview(titleLabel)
+        view.addSubview(imageView)
         view.addSubview(usernameTextField)
         view.addSubview(passwordTextField)
         view.addSubview(loginButton)
@@ -75,6 +117,9 @@ class LoginViewController: UIViewController {
             
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             usernameTextField.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 50),
             usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -88,7 +133,6 @@ class LoginViewController: UIViewController {
             loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
-    
     private func setupActions() {
         //이메일 텍스트필드와 emailObserver를 데이터 바인딩을 하는 코드
         usernameTextField.rx.text
@@ -135,7 +179,6 @@ class LoginViewController: UIViewController {
             }
         }).disposed(by: disposeBag)
     }
-    
     private func setGradient() -> CAGradientLayer{
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [UIColor.black.cgColor, UIColor(red: 41/255, green: 94/255, blue: 166/255, alpha: 1).cgColor]
@@ -145,6 +188,4 @@ class LoginViewController: UIViewController {
         
         return gradientLayer
     }
-    
-    
 }
