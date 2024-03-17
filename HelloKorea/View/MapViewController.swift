4950a21307af9a108179a4f4316487fbd2703ade
DongjuPark <pdj95044@gmail.com>
2024-03-17 19:06:50 +0900

    :bulb: MapKit 메소드 관련 추석 추가

--- HelloKorea/View/MapViewController.swift
+++ HelloKorea/View/MapViewController.swift
@@ -91,11 +91,13 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController: MKMapViewDelegate{
+    //지도의 표시 영역이 변경되기 직전에 호출
     func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
             if let selectedAnnotations = mapView.selectedAnnotations as? [MKPointAnnotation], let selectedAnnotation = selectedAnnotations.first {
                 mapView.deselectAnnotation(selectedAnnotation, animated: true)
             }
         }
+    //annotation 정의하는 메소드
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
         guard let annotation = annotation as? Artwork else {
           return nil
