85785e3dc79d6ce03e4e9cae59ba0f75af0cfb83
DongjuPark <pdj95044@gmail.com>
2024-03-27 15:05:32 +0900

    ✨💩 annotation 유형에 따른 annotation 이미지 유형 다르게 출력, 하지만  "dequeueReusableAnnotationView" 관련 이슈로 refactoring 필요

--- HelloKorea/View/map/CustomAnnotationView.swift
+++ HelloKorea/View/map/CustomAnnotationView.swift
@@ -8,16 +8,27 @@
 import MapKit
 
 class CustomAnnotationView: MKAnnotationView {
-    override var annotation: MKAnnotation?{
-        didSet{
-            if let annotation = annotation as? Artwork {
-                
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        var originalImage = UIImage()
+        if let annotation = annotation as? Location {
+            switch annotation.foodOrPray{
+            case "food":
+                originalImage = #imageLiteral(resourceName: "halal")
+            case "pray":
+                originalImage = #imageLiteral(resourceName: "Image")
+            default:
+                originalImage = UIImage()
             }
-            let originalImage = #imageLiteral(resourceName: "halal")
             let resizedImage = resizeImage(image: originalImage, newWidth: 35)
             image = resizedImage
         }
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
         let scale = newWidth / image.size.width
         let newHeight = image.size.height * scale
